a82b9bf157a33e2ef060223fc0f40ef25e72894d
Chadan <chandannadve@gmail.com>
2023-05-07 20:14:13 +0400

    My First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## End to End ML Project
+### Step 1
+### Created a Environment
+```
+conda create -p venv python==3.8
+
+conda activate venv/
+```
+### Step 2
+### Created README.md
+
+### Step 3
+### Created requirements.txt and add the neccessary linrary
+### Install using below command
+```
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHON_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_object:
+        requirements=file_object.readlines()
+        [req.replace("\n","") for req in requirements]
+        if HYPHON_E_DOT in requirements:
+            requirements.remove(HYPHON_E_DOT)
+    return requirements
+
+setup(
+    name='RegressorProject',
+    version='0.0.1',
+    author='Chandan',
+    author_email='chandannadve@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages()
+)
